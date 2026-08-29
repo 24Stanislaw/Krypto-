444c261538666bc5146f3277a4ce5e5cb9fe8ac0
24Stanislaw <stanislawignacyrybarski@gmail.com>
2026-08-29 17:45:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import requests
 import streamlit as st
 import os
-if os.path.exists("signals_history.csv"):
-    os.remove("signals_history.csv")
 
 # ==========================================
 # KONFIGURACJA STRONY I LOGOWANIE
